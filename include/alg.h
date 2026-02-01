f5793b7785bd5185123468f20e70ff011e91b602
anton <ashtanyuk@gmail.com>
2026-02-01 19:05:07 +0300

    Начальный коммит

---
+++ include/alg.h
@@ -0,0 +1,12 @@
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
+#include <cstdint>
+
+bool checkPrime(uint64_t value);
+uint64_t nPrime(uint64_t n);
+uint64_t nextPrime(uint64_t value);
+uint64_t sumPrime(uint64_t hbound);
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound);
+
+#endif  // INCLUDE_ALG_H_
